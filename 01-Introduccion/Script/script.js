be99beadda10d65ba7d415d4bba2d71c77aa7450
euclidesseg <euder0206@gmail.com>
2022-10-18 23:01:01 -0500

    =====

--- 01-Introduccion/Script/script.js
+++ 01-Introduccion/Script/script.js
@@ -1,2 +1,19 @@
 /* Estaremos viendo la forma de como pasar o mover JavasCript a su propio archivo*/
 alert("Hola mundo")
+
+// para ver cuanto tiempo se demora un codigo en ejecutar 
+
+console.time("hola");
+
+console.warn("esto no esta permitido");
+console.warn("esto no esta permitido");
+console.warn("esto no esta permitido");
+console.warn("esto no esta permitido");
+console.warn("esto no esta permitido");
+console.warn("esto no esta permitido");
+console.warn("esto no esta permitido");
+console.warn("esto no esta permitido");
+console.warn("esto no esta permitido");
+console.warn("esto no esta permitido");
+
+console.timeEnd("hola");
