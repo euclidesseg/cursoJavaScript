7b94b17524a89afe709ac6b682433fca1c4a5467
euclidesseg <euder0206@gmail.com>
2022-11-29 07:19:39 -0500

    pppppp

--- 13-DOM/js/01-scripts-acceder-a-elementos-de-html.js
+++ 13-DOM/js/01-scripts-acceder-a-elementos-de-html.js
@@ -3,12 +3,37 @@
 
 // Document es basiamente todo el html que tenemos en nuestro proyecto
 
-let elemento
+let elemento;
 
-elemento = document
-elemento = document.all
+elemento = document;
+// Siempre se va usar document para seleccionar un elemento del html
+elemento = document.all;
+// Que seleccione todos los elementos que conforman el hml
+elemento = document.head;
+// head se va a referir a la parte superior del documento donde estan las etiquetas metas
+// y los linsk en general
+elemento = document.domain;
+// Seleccionar el dominio actual
+elemento = document.forms;
+// Acceder a los formularios del elemento
+elemento = document.forms[0]
+// Accediento al primer elemento ya que nos retorna un arreglo y puedo acceder a ellos
+// por medios del indice
+elemento = document.forms[0].id;
+// accediendo al un id si lo hay
+elemento = document.forms[0].method
+// accediento al metodo
+elemento = document.forms[0].classList;
+// accediendo a cada una de las clases del formulario
+elemento = document.forms[0].action;
 
-
+elemento = document.links;
+// eso me retorna un arreglo con todos los enlaces agregados como elementos
+elemento = document.links[15].href
+// seleccionando un indice del arreglo
+elemento = document.images;
+// para saber cuantas imagenes estamos uzando en nuestro proyecto
+elemento = document.scripts
 
 
 console.log(elemento);
